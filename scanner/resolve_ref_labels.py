798c01f38505d9d40e2e72ed1d1c883e2bb33f70
mike <msgoff381@gmail.com>
2026-06-10 14:59:58 +0000

    resolve ref labels and cite bibitems

--- scanner/resolve_ref_labels.py
+++ scanner/resolve_ref_labels.py
@@ -48,6 +48,18 @@ cites.tok = cites.tok.apply(lambda x: re.sub(">}$", "", str(x)))
 unique_cites = set(cites["tok"].tolist())
 unique_cites = [x.replace("\\cite{", "") for x in unique_cites]
 unique_cites = set([re.sub("}$", "", str(x)) for x in unique_cites])
+remove = set()
+add2 = set()
+for cite in unique_cites:
+    if "," in cite:
+        for c in cite.split(","):
+            add2.add(c)
+    remove.add(cite)
+for cite in remove:
+    unique_cites.remove(cite)
+
+for cite in add2:
+    unique_cites.add(cite)
 print(unique_cites)
 
 resp = re.findall(r"\\begin{thebibliography}.*?\\end{thebibliography}", data, re.DOTALL)
@@ -55,4 +67,7 @@ resp = resp[0]
 resp = re.split(r"(\\bibitem{.*?})", resp)
 bibitems = [x.replace("\\bibitem{", "") for x in resp if "bibitem" in str(x)]
 bibitems = [re.sub("\}$", "", str(x)) for x in bibitems]
-bibitems = list(set(bibitems))
+unique_bibitems = set(bibitems)
+
+print("unresolved", unique_cites.difference(unique_bibitems))
+print("unresolved", unique_bibitems.difference(unique_cites))
